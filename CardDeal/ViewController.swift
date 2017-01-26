0a5f5043361267392f2c68d96fbec060f304ec0c
Madhur Toppo <madhurtoppo@Madhurs-MacBook-Pro.local>
2017-01-26 15:04:21 -0500

    Initial Commit

---
+++ CardDeal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CardDeal
+//
+//  Created by Madhur Toppo on 1/26/17.
+//  Copyright © 2017 Madhur Toppo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
