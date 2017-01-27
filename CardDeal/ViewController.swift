4720c4fd35796bfabe80cf3aa3145b16bfe3e1eb
Madhur Toppo <madhurtoppo@Madhurs-MacBook-Pro.local>
2017-01-26 23:58:14 -0500

    Added logic for deal button tapped

--- CardDeal/ViewController.swift
+++ CardDeal/ViewController.swift
@@ -14,12 +14,22 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    @IBOutlet weak var rightImageView: UIImageView!
+    @IBOutlet weak var leftImageView: UIImageView!
+    @IBOutlet weak var leftScoreLabel: UILabel!
+    @IBOutlet weak var rightScoreLabel: UILabel!
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func dealTapped(sender: UIButton) {
+        print("deal Tapped")
+        
+        leftImageView.image = UIImage(named: "card10")
+        rightImageView.image = UIImage(named: "jack")
 
+    }
 }
 
